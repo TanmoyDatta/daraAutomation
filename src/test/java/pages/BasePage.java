8a45805d18333d5f51d77795a8df2307af9a91b9
Tanmoy Datta <tonmoydatta45@gmail.com>
2023-02-19 22:58:32 +0600

    web automated a site

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -4,12 +4,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.qameta.allure.Allure;
 
 import static utilities.DriverSetUp.getDriver;
 
 import java.io.ByteArrayInputStream;
+import java.time.Duration;
+import java.util.List;
 
 public class BasePage {
 	
@@ -36,4 +40,10 @@ public class BasePage {
 	public void takeScreenShot(String name) {
 		Allure.addAttachment(name, new ByteArrayInputStream(((TakesScreenshot)getDriver()).getScreenshotAs(OutputType.BYTES) ));
 	}
+	
+	public List<WebElement> getVisibleElements(By locator) {
+		WebDriverWait wait = new WebDriverWait(getDriver(),Duration.ofSeconds(30));
+		return wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(locator));
+	}
 }
+
